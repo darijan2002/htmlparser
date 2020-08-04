52ecba4dd62ab600373bdee9765878e6db87255c
Darijan Sekerov <darijan2002@gmail.com>
2020-08-04 22:37:14 +0200

    trim excess whitespace and newlines get indexes of all '<' characters

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use std::env;
+use std::io;
+use std::fs;
+use std::ops::Deref;
+use std::str::FromStr;
 
 fn main() {
     let vec: Vec<String> = env::args().collect();
@@ -10,4 +14,15 @@ fn main() {
         args.iter().all(|s| s.ends_with(".html")),
         "invalid arguments"
     );
+
+    args.iter().for_each(|x| {
+        let mut content = fs::read_to_string(x).expect(format!("error reading {:?}", x).as_str());
+        content = content.lines().map(|line| line.trim().chars()).flatten().collect();
+        println!("contents of {:?}", x);
+        println!("{:?}", content);
+        // let mut start;
+        println!("{:?}", content.chars().enumerate().filter(|&c| c.1 == '<').collect::<Vec<(usize, char)>>());
+
+
+    });
 }
